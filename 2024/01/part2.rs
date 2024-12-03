7ea6465a4be286149c2c4a72f7fed44a08d393b1
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2024-12-03 16:58:09 +0100

    Converted 2024 day 1 part 2 from Python into Rust

--- 2024/01/part2.rs
+++ 2024/01/part2.rs
@@ -1,3 +1,34 @@
+use std::{collections::HashMap, env, fs::File, io, io::BufRead, path::Path};
+
+fn read_input(path: &Path) -> io::Result<Vec<(i64, i64)>> {
+    let file = File::open(path)?;
+    let mut data: Vec<(i64, i64)> = Vec::new();
+    for line in io::BufReader::new(file).lines() {
+        let values = Vec::from_iter(line?.split_whitespace().map(
+            |s| s.parse().expect("values must be integers")
+        ));
+        match &values[..] {
+            &[first, second, ..] => data.push((first, second)),
+            _ => panic!("input must have two values per line"),
+        };
+    }
+    Ok(data)
+}
+
+fn solve(data: &Vec<(i64, i64)>) {
+    let mut freqs: HashMap<i64, usize> = HashMap::new();
+    for (_, x) in data {
+        *freqs.entry(*x).or_insert(0) += 1
+    }
+    let total_delta: usize = data.iter().map(|(x, _)| *freqs.get(x).unwrap()).sum();
+    println!("{total_delta}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    for arg in &args[1..] {
+        println!("=== {arg}");
+        let data = read_input(Path::new(arg)).expect("reading input");
+        solve(&data);
+    }
 }
