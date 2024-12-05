94477fcb1a7e2878750a6aa47729a80642655b1b
Maarten ter Huurne <maarten@treewalker.org>
2024-12-05 23:07:53 +0100

    Simplify day 5 even more
    We don't even need to build the correct order, as the histogram
    contains the same information.

--- 2024/05/both.py
+++ 2024/05/both.py
@@ -24,21 +24,19 @@ def solve(rules, books):
     score2 = 0
     for book in books:
         # Use the fact that the input compares every page combination.
+        # The number of times each page occurs in the right hand side of
+        # a comparison rule is equal to its position in the correct order.
         freqs = {page: 0 for page in book}
         for p1, p2 in rules:
             if p1 in freqs and p2 in freqs:
                 freqs[p2] += 1
-        correct = tuple(sorted(freqs, key=freqs.__getitem__))
-        print(book)
-        print(correct)
-        middle = correct[len(book) // 2]
-        if book == correct:
+        middle, = (p for p, c in freqs.items() if c == len(book) // 2)
+        if all(p == book[c] for p, c in freqs.items()):
             print("right order, middle page is", middle)
             score1 += middle
         else:
             print("wrong order, middle page should be", middle)
             score2 += middle
-        print()
     print(f"{score1=}")
     print(f"{score2=}")
 
