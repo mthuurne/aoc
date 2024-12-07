5036821f58d40802b83a9d694cd8aa98402905fb
Maarten ter Huurne <maarten@treewalker.org>
2024-12-07 10:52:35 +0100

    Solved 2024 day 7 part 2

---
+++ 2024/07/part2.py
@@ -0,0 +1,36 @@
+import fileinput
+
+
+def read_input():
+    for line in fileinput.input():
+        target_str, *value_strs = line.split()
+        assert target_str.endswith(":")
+        target = int(target_str[:-1])
+        values = tuple(int(s) for s in value_strs)
+        yield target, values
+
+
+def can_match(target, values):
+    num_values = len(values)
+
+    def rec(idx, total):
+        if idx == num_values:
+            return total == target
+        if total > target:
+            # Our operators can only increase the total, so we can't match from here.
+            return False
+        value = values[idx]
+        return rec(idx + 1, total * value) or rec(idx + 1, total + value) or rec(
+            idx + 1, int(f"{total}{value}")
+        )
+
+    return rec(1, values[0])
+
+
+def solve(equations):
+    total = sum(target for target, values in equations if can_match(target, values))
+    print(total)
+
+
+if __name__ == "__main__":
+    solve(tuple(read_input()))
