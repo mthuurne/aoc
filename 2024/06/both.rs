b399dc74e5d8fe9364767ebb17afac398ba3ad73
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2024-12-08 00:52:56 +0100

    Use `usize` instead of `i64`
    I thought using signed integers would simplify updating the coordinates
    during simulation, but it complicates everything else, so that is not
    a net benefit.

--- 2024/06/both.rs
+++ 2024/06/both.rs
@@ -1,21 +1,21 @@
 use std::{collections::HashSet, env, fs::File, io::{self, BufRead}, path::Path};
 
-fn read_input(path: &Path) -> io::Result<(HashSet<(i64, i64)>, (i64, i64), (i64, i64))> {
+fn read_input(path: &Path) -> io::Result<(HashSet<(usize, usize)>, (usize, usize), (usize, usize))> {
     let file = File::open(path)?;
 
-    let mut start = (-1, -1);
+    let mut start = (usize::max_value(), usize::max_value());
     let mut obstacles = HashSet::new();
     let mut height = 0;
-    let mut width = -1;
+    let mut width = 0;
     for (y, line) in io::BufReader::new(file).lines().enumerate() {
         let row = line?;
         height += 1;
-        width = row.len() as i64;
+        width = row.len();
         for (x, cell) in row.chars().enumerate() {
             match cell {
-                '.' => {},
-                '#' => { obstacles.insert((x as i64, y as i64)); },
-                '^' => start = (x as i64, y as i64),
+                '.' => (),
+                '#' => { obstacles.insert((x, y)); },
+                '^' => start = (x, y),
                 _ => panic!("unexpected char in grid"),
             }
         }
@@ -24,16 +24,16 @@ fn read_input(path: &Path) -> io::Result<(HashSet<(i64, i64)>, (i64, i64), (i64,
     Ok((obstacles, start, (width, height)))
 }
 
-fn simulate(obstacles: &HashSet<(i64, i64)>, start: (i64, i64), size: (i64, i64)) -> Option<HashSet<(i64, i64, i64, i64)>> {
+fn simulate(obstacles: &HashSet<(usize, usize)>, start: (usize, usize), size: (usize, usize)) -> Option<HashSet<(usize, usize, i64, i64)>> {
     let mut visited = HashSet::new();
     let mut guard = (start.0, start.1, 0, -1);
     while visited.insert(guard) {
         let (x, y, dx, dy) = guard;
-        let nx = x + dx;
-        let ny = y + dy;
+        let nx = x.wrapping_add(dx as usize);
+        let ny = y.wrapping_add(dy as usize);
         if obstacles.contains(&(nx, ny)) {
             guard = (x, y, -dy, dx);
-        } else if 0 <= nx && nx < size.0 && 0 <= ny && ny < size.1 {
+        } else if nx < size.0 && ny < size.1 {
             guard = (nx, ny, dx, dy);
         } else {
             return Some(visited);
@@ -42,7 +42,7 @@ fn simulate(obstacles: &HashSet<(i64, i64)>, start: (i64, i64), size: (i64, i64)
     None
 }
 
-fn solve(obstacles: HashSet<(i64, i64)>, start: (i64, i64), size: (i64, i64)) {
+fn solve(obstacles: HashSet<(usize, usize)>, start: (usize, usize), size: (usize, usize)) {
     let mut visited = simulate(&obstacles, start, size).unwrap().iter().map(
         |(x, y, _dx, _dy)| (*x, *y)
     ).collect::<HashSet<_>>();
