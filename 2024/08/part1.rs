72cae8b9c49c99365a64dd599840b761ba1300b3
Maarten ter Huurne <maarten.terhuurne@protix.eu>
2024-12-09 12:01:59 +0100

    Converted 2024 day 8 part 1 from Python into Rust

---
+++ 2024/08/part1.rs
@@ -0,0 +1,54 @@
+use itertools::Itertools;
+use std::{collections::{HashMap, HashSet}, env, fs::File, io::{self, BufRead}, path::Path};
+
+fn read_input(path: &Path) -> io::Result<(usize, usize, HashMap<char, HashSet<(usize, usize)>>)> {
+    let file = File::open(path)?;
+
+    let mut antennas = HashMap::new();
+    let mut height = 0usize;
+    let mut width = 0;
+    for (y, line) in io::BufReader::new(file).lines().enumerate() {
+        let text = line?;
+        height += 1;
+        width = text.len();
+        for (x, ch) in text.chars().enumerate() {
+            if ch != '.' {
+                antennas.entry(ch).or_insert_with(|| HashSet::new()).insert((x, y));
+            }
+        }
+    }
+
+    Ok((width, height, antennas))
+}
+
+
+fn solve(width: usize, height: usize, antennas: HashMap<char, HashSet<(usize, usize)>>) {
+    let mut antinodes = HashSet::new();
+
+    let mut add_antinode = |x: isize, y: isize| {
+        if 0 <= x && x < width as isize && 0 <= y && y < height as isize {
+            antinodes.insert((x, y));
+        }
+    };
+
+    for locations in antennas.values() {
+        for combo in locations.iter().combinations(2) {
+            let (x1, y1) = combo[0];
+            let (x2, y2) = combo[1];
+            let dx = *x2 as isize - *x1 as isize;
+            let dy = *y2 as isize - *y1 as isize;
+            add_antinode(*x1 as isize - dx, *y1 as isize - dy);
+            add_antinode(*x2 as isize + dx, *y2 as isize + dy);
+        }
+    }
+
+    println!("{}", antinodes.len());
+}
+
+fn main() {
+    for arg in env::args().skip(1) {
+        println!("=== {arg}");
+        let (width, height, antennas) = read_input(Path::new(&arg)).expect("reading input");
+        solve(width, height, antennas);
+    }
+}
