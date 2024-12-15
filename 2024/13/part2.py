3b4779486da5981d32172b9f59765fc5dad7da57
Maarten ter Huurne <maarten@treewalker.org>
2024-12-15 04:29:49 +0100

    Use way simpler approach for day 13 part 2
    My original approach was a lot more complicated than required.

--- 2024/13/part2.py
+++ 2024/13/part2.py
@@ -1,5 +1,4 @@
 import fileinput
-from math import gcd
 
 
 def read_input():
@@ -26,81 +25,31 @@ def read_input():
             yield a_offset, b_offset, prize
 
 
-def extended_euclid(a, b):
-    """Return (g, x, y) such that g = gcd(a, b) and a * x + b * y = g."""
-    ro, r = a, b
-    so, s = 1, 0
-    to, t = 0, 1
-    while True:
-        q, m = divmod(ro, r)
-        if m == 0:
-            return r, s, t
-        ro, r = r, m
-        so, s = s, so - q * s
-        to, t = t, to - q * t
-
-
-def solve1d(a, b, n):
-    """
-    Given positive integers a, b, n, return factors x and y and steps i and j,
-    such that a(x - ki) + b(y + kj) = n.
-    If no solution exists, None is returned.
-    """
-
-    # Remove common dividers shared by a, b and n.
-    g = gcd(a, b, n)
-    a //= g
-    b //= g
-    n //= g
-
-    g, x1, y1 = extended_euclid(a, b)
-    if g != 1:
-        # As a and b have a common divider, every ax + by will have that divider,
-        # but n does not have it, so there is no solution.
-        return None
-
-    # Since x1 * a + y1 * b = 1, multiply both factors by n to get n as a result.
-    return x1 * n, y1 * n, b, a
-
-
-# def solve(machines, offset=0):
 def solve(machines, offset=10000000000000):
     total_cost = 0
 
     for (ax, ay), (bx, by), (px, py) in machines:
         px += offset
         py += offset
-        sx = solve1d(ax, bx, px)
-        sy = solve1d(ay, by, py)
-        if sx is None or sy is None:
+
+        if by * ax == bx * ay:
+            # The vectors are parallel.
+            # We have to push the button that is cheapest per distance travelled
+            # as many times as possible without overshooting the target and then
+            # the other button zero or more times to reach the target.
+            # However, this case does not occur in our input and therefore
+            # we do not handle it.
+            assert False
+
+        nb = (py * ax - ay * px) // (by * ax - bx * ay)
+        na = (px - bx * nb) // ax
+        if na < 0 or nb < 0:
+            # Both vectors point below or above the target, so we cannot reach it.
+            # This doesn't occur in our input either, but is trivial to handle.
             continue
-        nax, nbx, iax, ibx = sx
-        nay, nby, iay, iby = sy
-
-        # ax * (nax - r * iax) + bx * (nbx + r * ibx) == px
-        # ay * (nay - s * iay) + by * (nby + s * iby) == py
-        # na == nax - r * iax == nay - s * iay
-        # nb == nbx + r * ibx == nby + s * iby
-
-        na = nax % iax
-        nb = (px - ax * na) // bx
-        assert ax * na + bx * nb == px
-        while nb >= 0:
-            s, t = divmod(nay - na, iay)
-            if t == 0:
-                assert na == nay - s * iay
-                if nby + s * iby == nb:
-                    break
-            na += iax
-            nb -= ibx
-            assert ax * na + bx * nb == px
-        else:
-            continue
-
-        assert ax * na + bx * nb == px
-        assert ay * na + by * nb == py
-        cost = 3 * na + 1 * nb
-        total_cost += cost
+        if ax * na + bx * nb == px and ay * na + by * nb == py:
+            # A single integer solution exists.
+            total_cost += 3 * na + nb
 
     print(total_cost)
 
