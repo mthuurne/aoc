9af3d0617af9a64ca06c7fe3a2f9de24b79fc48b
Maarten ter Huurne <maarten@treewalker.org>
2024-12-19 09:17:46 +0100

    Solved 2024 day 19 part 1

---
+++ 2024/19/part1.py
@@ -0,0 +1,77 @@
+import fileinput
+
+
+def read_input():
+    lines = fileinput.input()
+
+    supply = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            break
+        for pattern in line.split(","):
+            supply.append(pattern.strip())
+
+    designs = []
+    for line in lines:
+        designs.append(line.strip())
+
+    return supply, designs
+
+
+class Node:
+    def __init__(self):
+        self.leaf = False
+        self.children = {}
+
+    def __repr__(self):
+        child_repr = ", ".join(
+            f"{color}: {node!r}"
+            for color, node in self.children.items()
+        )
+        return f"[{child_repr}]" if self.leaf else f"({child_repr})"
+
+    def add_pattern(self, pattern):
+        if pattern:
+            color = pattern[0]
+            if (child := self.children.get(color)) is None:
+                child = Node()
+                self.children[color] = child
+            child.add_pattern(pattern[1:])
+        else:
+            self.leaf = True
+
+    def iter_prefix_lengths(self, design):
+        if self.leaf:
+            yield 0
+        if design:
+            color = design[0]
+            child = self.children.get(color)
+            if (child := self.children.get(color)) is not None:
+                for length in child.iter_prefix_lengths(design[1:]):
+                    yield length + 1
+
+
+def is_possible(tree, design):
+    full = len(design) + 1
+    reachable = [False] * full
+    reachable[0] = True
+    for idx in range(len(design)):
+        if reachable[idx]:
+            for length in tree.iter_prefix_lengths(design[idx:]):
+                if (end := idx + length) < full:
+                    reachable[end] = True
+    return reachable[-1]
+
+
+def solve(supply, designs):
+    tree = Node()
+    for pattern in supply:
+        tree.add_pattern(pattern)
+
+    num_possible = sum(is_possible(tree, design) for design in designs)
+    print(num_possible)
+
+
+if __name__ == "__main__":
+    solve(*read_input())
